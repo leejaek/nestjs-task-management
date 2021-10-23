b3043de4f0cbc989d976ce36d4b9c5d69ef6f224
leejaek <leejh9049@gmail.com>
2021-10-23 23:55:19 +0900

    Implementing JWT Validation

--- src/auth/users.repository.ts
+++ src/auth/users.repository.ts
@@ -9,7 +9,6 @@ export class UserRepository extends Repository<User> {
     async createUser(authCredentialsDto: AuthCredentialsDto): Promise<void> {
         const { username, password } = authCredentialsDto;
 
-        // hash
         const salt = await bcrypt.genSalt();
         const hashedPassword = await bcrypt.hash(password, salt);
 
