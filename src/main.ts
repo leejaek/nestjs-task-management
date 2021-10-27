82a9227064ea60cc05824449a7ef59c7bb34cb16
leejaek <leejh9049@gmail.com>
2021-10-27 13:06:11 +0900

    Apply Fastify

--- src/main.ts
+++ src/main.ts
@@ -3,17 +3,26 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { TransformInterceptor } from './transform.interceptor';
 import { Logger } from '@nestjs/common';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
 
 async function bootstrap() {
   const logger = new Logger();
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter({
+      logger: true,
+    }),
+  );
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalInterceptors(new TransformInterceptor());
 
   const port = process.env.PORT;
 
-  await app.listen(port);
+  await app.listen(port, '0.0.0.0');
 
   logger.log(`Application listening on port ${port}`);
 }
