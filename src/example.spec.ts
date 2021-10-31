e051095fffb42b88ba442fa92d68ea74b2d71d5a
leejaek <leejh9049@gmail.com>
2021-10-31 21:10:36 +0900

    Fixing import paths

--- src/example.spec.ts
+++
@@ -1,9 +0,0 @@
-function addNumbers(num1, num2) {
-  return num1 + num2;
-}
-
-describe('addNumbers', () => {
-  it('adds two Numbeers', () => {
-    expect(addNumbers(2, 2)).toEqual(4);
-  });
-});
